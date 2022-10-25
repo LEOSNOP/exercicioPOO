dcd2a7e96164bedb8b3505b11cbb334edc55f23f
_L_ <conceicao.leonardo9@gmail.com>
2022-10-25 10:05:29 -0300

    quartin

--- src/Pratica/teste.java
+++ src/Pratica/teste.java
@@ -1,4 +1,12 @@
 package Pratica;
 
 public class teste {
+    private Gafanhoto exib1 ;
+    private Video exib2;
+    public void exibir (){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
 }
